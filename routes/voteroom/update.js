63d3252391aa214dfdd5d3cef099650649cccb94
devopskims <skywon1115@gmail.com>
2022-03-04 13:52:20 +0900

    env

--- routes/voteroom/update.js
+++ routes/voteroom/update.js
@@ -1,7 +1,45 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+  fastify.patch('/voteroom/:id/vote', async function (request, reply) {
+   // const getinfo = async (authorization) => const {data} = await APi.get ('/voteroom/:id') 
+    const result = await update(mogoDB,request.params.id, request.body)
+    
+    req headers
+        {
+          "authorization": "authorization token"
+          
+        }
+        req body
+        {
+          "choice_list": {
+              "돼지국밥": 1,
+              "김치찌개": 4
+          }
+        } {
+          return reply
+          .code (200 )
+          .header ('Content-Type', 'application/json', 'charset=utf-8')
+          .send ({"value" :result })
+        }
+
+
+//     
+    // 투표 항목에 투표 응답 바디
+
+//     res body
+
+//     {
+//       "title": "점심메뉴",
+//       "choice_list": {
+//             "돼지국밥": 1,
+//             "김치찌개": 4
+//       }
+//       "multi_choice": true,
+//       "startDate": 2022-03-03,
+//       "periodDay": 1
+//     }
+
+
+//   })
+// }
