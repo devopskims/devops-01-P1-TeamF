78a868b9e091623589efbc1cae4fab7fcf86bcc4
KKHYeon <kang01019@gmail.com>
2022-03-06 14:21:49 +0900

    read api 구현

--- routes/voteroom/read.js
+++ routes/voteroom/read.js
@@ -1,7 +1,55 @@
 'use strict'
 
+const { readAll, readOne, readPublicOne, readPrivateOne } = require('../../model')
+
 module.exports = async function (fastify, opts) {
+
+  // 전체 조회
   fastify.get('/', async function (request, reply) {
-    return 'this is an example'
+    const result = await readAll(this.mongo)
+    
+    reply
+      .code(200)
+      .header('content-type', 'application/json')
+      .send(result) 
+  })
+
+
+  fastify.get('/:id', async function (request, reply) {
+    const result = await readPublicOne(this.mongo, request.params.id)
+
+    if (result.private){
+      reply
+      .code(404)
+      .header()
+      .send(result)
+    }
+    else{
+      reply
+      .code(200)
+      .header('content-type', 'application/json')
+      .send(result)
+    }
+  })
+
+  // 비공개 투표 1개 조회
+  fastify.post('/:id', async function (request, reply) {
+    console.log(request.body.password)
+    const result = await readPrivateOne(this.mongo, request.params.id, request.body.password)
+    console.log(result)
+    if (result){
+      reply
+      .code(200)
+      .header('content-type', 'application/json')
+      .send(result)
+    }
+    else {
+      reply
+      .code(404)
+      .header()
+      .send(result)
+    }
+
+
   })
 }
