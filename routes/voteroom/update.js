841621604e736be1c86c36f774d4be1eac3c1b35
KKHYeon <kang01019@gmail.com>
2022-03-06 15:54:23 +0900

    endpoint 수정

--- routes/voteroom/update.js
+++ routes/voteroom/update.js
@@ -3,17 +3,14 @@ const { updateOne } = require ('../../model')
 
 module.exports = async function (fastify,opts) {
 
-    
-fastify.patch ('/voteroom/:id/vote', async function (request, reply) { 
-    const result = await updateOne (this.mongo,request.params.id,request.body )
+    fastify.patch ('/:id/vote', async function (request, reply) { 
+        const result = await updateOne (this.mongo, request.params.id, request.body)
 
-    reply
-    .code (200 )
-    .header ('Content-Type', 'application/json', 'charset=utf-8')
-    .send (result)
-
-
-})
+        reply
+        .code(200)
+        .header('Content-Type', 'application/json', 'charset=utf-8')
+        .send(result)
+    })
 }
 
 
