4da5d0f759ac2fb9a11622959462f1f849c15eed
jangsanghyen <jsh732386@gmail.com>
2022-03-06 16:21:43 +0900

    create

--- routes/voteroom/create.js
+++ routes/voteroom/create.js
@@ -1,7 +1,16 @@
 'use strict'
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
+const { createOne } = require('../../model')
+
+
+module.exports = async function (app, opts) {
+  app.post('/', async function (request, reply) {
+   
+   const result = await createOne(this.mongo, request.body)
+   
+ reply
+    .code(200) 
+    .header('content-type', 'application/json')
+    .send(result) 
   })
 }
