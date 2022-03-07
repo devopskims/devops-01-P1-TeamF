e36a01f81223e9d5d4a53235520f81dc80c23e8e
jangsanghyen <jsh732386@gmail.com>
2022-03-07 10:33:30 +0900

    create

--- routes/voteroom/create.js
+++ routes/voteroom/create.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const { createOne } = require('../../model')
+const { readOne, createOne } = require('../../model')
 
 
 module.exports = async function (app, opts) {
   app.post('/', async function (request, reply) {
    
    const result = await createOne(this.mongo, request.body)
-   
+   comsole.log(result)
  reply
     .code(200) 
     .header('content-type', 'application/json')
