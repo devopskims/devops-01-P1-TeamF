c20cd7566a184df71babc199b4607b1b20a93005
KKHYeon <kang01019@gmail.com>
2022-03-07 16:20:48 +0900

    delete response body 수정

--- routes/voteroom/delete.js
+++ routes/voteroom/delete.js
@@ -1,18 +1,19 @@
 'use strict'
 
-const { deleteOne } = require('../../model')
+const { deleteOne, readOne } = require('../../model')
 
 module.exports = async function (fastify, opts) {
 
   // 전체 조회
   fastify.delete('/:id/delete', async function (request, reply) {
-    const id = request.id
+    const id = request.params.id
+    const sendResult = await readOne(this.mongo, id)
     const result = await deleteOne(this.mongo, id)
     
     reply
       .code(200)
       .header('content-type', 'application/json')
-      .send(result) 
+      .send(sendResult) 
   })
   
 }
