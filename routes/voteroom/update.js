53cddaf589a5c7884b1e5b53a1ce10be2f5ba893
devopkim <98450173+devopskims@users.noreply.github.com>
2022-03-07 11:22:44 +0900

    Update update.js

--- routes/voteroom/update.js
+++ routes/voteroom/update.js
@@ -1,22 +1,25 @@
 'use strict'
-const { updateOne } = require ('../../model')
+const { readPublicOne, readPrivateOne, updateOne } = require ('../../model')
 
 module.exports = async function (fastify,opts) {
 
     fastify.patch ('/:id/vote', async function (request, reply) { 
         const result = await updateOne (this.mongo, request.params.id, request.body)
+        const id = request.params.id
+        
+        if(result.private){
+            const modify = await readPrivateOne (this.mongo, request.params.id) 
+            reply
+            .code(200)
+            .header('Content-Type', 'application/json', 'charset=utf-8')
+            .send(modify)
+        } else{
+            const modify = await readPublicOne (this.mongo, request.params.id, request.body.password)
+            reply
+            .code(200)
+            .header('Content-Type', 'application/json', 'charset=utf-8')
+            .send(modify)
+            }
 
-        reply
-        .code(200)
-        .header('Content-Type', 'application/json', 'charset=utf-8')
-        .send(result)
     })
 }
-
-
-
-
-
-
-  
-
